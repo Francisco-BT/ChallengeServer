e8101222563ee0bf9234ef0d4c5e6c0af8b9e2f1
Francisco-BT <pancho10810@gmail.com>
2021-09-14 11:22:25 -0500

    Feat: integration test to not create an user

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,9 +1,12 @@
 const { UserController } = require('../../controllers');
 const { User } = require('../../models');
+const { newUserValidator } = require('../../middlewares');
 
 const controller = new UserController(User);
 module.exports = (router) => {
-  router.post('/', (req, res) => controller.create(req, res));
+  router.post('/', newUserValidator(), (req, res) =>
+    controller.create(req, res)
+  );
 
   return router;
 };
--- test/routes/users.spec.js
+++ test/routes/users.spec.js
@@ -12,13 +12,17 @@ beforeEach(async () => {
 
 describe('Users API', () => {
   describe('POST - /api/v1/users', () => {
-    it('should create a new user', async () => {
+    async function postUser(user) {
+      return await request(app).post('/api/v1/users').send(user);
+    }
+
+    it('should create a new user with only name, email and password', async () => {
       const user = {
         name: 'Francisco',
         email: 'fbernabe@test.com',
-        password: 'test',
+        password: 'P4ssw0rd',
       };
-      const response = await request(app).post('/api/v1/users').send(user);
+      const response = await postUser(user);
 
       expect(response.status).toBe(201);
       expect(Object.keys(response.body)).toEqual([
@@ -31,17 +35,15 @@ describe('Users API', () => {
       ]);
     });
 
-    it.skip('should not create a user with missing fields', async () => {
-      const user = {
-        name: 'Francisco',
-        englishLevel: 'B2',
-      };
-      const response = await request(app).post('/api/v1/users').send(user);
+    it('should not create a user with missing password, email or name', async () => {
+      const response = await postUser({ englishLevel: 'B2' });
       expect(response.status).toBe(400);
-      expect(response.body.errors).toEqual([
-        'Email cannot be null and needs to have the format xxxx@xxxx.com',
-        'Password cannot be null and needs to have 8 or more characters',
-      ]);
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors).toEqual(
+        expect.objectContaining({ password: 'Password cannot be null' }),
+        expect.objectContaining({ email: 'Email cannot be null' }),
+        expect.objectContaining({ name: 'Name cannot be null' })
+      );
     });
   });
 });
